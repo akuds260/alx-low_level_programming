d69c542bbf9099d8dcb4bf4bf26dcb9f3104ab19
akuds260 <melakugirma2014@gmail.com>
2023-07-20 06:05:10 -0700

    a function that prints a triangle, followed by a new

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * print_square - prints square
- * @size: param
+ * print_square - prints a square, followed by a new line
+ * @size: size of the square
+ * Return: void
  */
-
 void print_square(int size)
 {
-	int i = 0, j;
-
-	if (size > 0)
-	{
-		for (; i < size; i++)
-		{
-			for (j = 0; j < size; j++)
-				_putchar(35);
-			_putchar('\n');
-		}
-	}
-	else
-	
+int i, j;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}	
