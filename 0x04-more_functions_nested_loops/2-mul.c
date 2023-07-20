f272692d0a91dcf1a7146a16f2f665ad266d0b93
akuds260 <melakugirma2014@gmail.com>
2023-07-20 05:26:26 -0700

    function that checks for a digit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,12 @@
-include "main.h"
+#include "main.h"
 
 /**
- * mul - multiplies two integers.
- * @a: first number.
- * @b: second number.
- * Return: multiplication.
+ * mul - multiplies 2 nums
+ * @a: first param
+ * @b: second param
+ * Return: something
  */
+
 int mul(int a, int b)
 {
 	return (a * b);
