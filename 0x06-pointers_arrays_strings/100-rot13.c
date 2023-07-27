039681895071a468fa3d143b136ab4665bb9bcd1
akuds260 <melakugirma2014@gmail.com>
2023-07-27 02:15:48 -0700

    a function that adds two numbers.

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,30 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * rot13 - encoding a string using rot13.
- * @s: the string to be encoded to rot13.
- * Return: the string s encoded to rot13.
- */
-
-char *rot13(char *s)
+int main(void)
 {
-	int i = 0, j = 0;
-	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
-	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	int n;
+	int a[5];
+	int *p;
 
-	while (s[i] != '\0')
-	{
-		do {
-			if (s[i] == string_alpha[j])
-			{
-				s[i] = string_rot13[j];
-				break;
-			}
-			j++;
-		} while (string_alpha[j] != '\0');
-		j = 0;
-		i++;
-	}
-
-	return (s);
+	a[2] = 1024;
+	p = &n;
+       	/*
+	* write your line of code here...
+	* Remember:
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
+	*/
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
